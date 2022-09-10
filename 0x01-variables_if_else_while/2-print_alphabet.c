ebb11f8ae57475eb785e0eb7c0f4a805b1a91681
calebtoo <calebtoo77@gmail.com>
2022-09-10 13:33:49 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Always Success)
+ */
+
+int main(void)
+
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+		
+	return(0);
+}
