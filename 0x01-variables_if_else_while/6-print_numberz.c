8685cf0ee72889198c50db89bcd3f050d5197663
calebtoo <calebtoo77@gmail.com>
2022-09-10 15:01:50 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - digits up to nine
+ * Return: 0 (Always Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+		
