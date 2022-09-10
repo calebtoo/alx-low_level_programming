d7b4be54923a6480f9dbf87a8f0e2637ef929dcd
calebtoo <calebtoo77@gmail.com>
2022-09-10 15:58:55 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - ascending single digit sep. by commas
+ * Return: 0 (Always Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+	
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
