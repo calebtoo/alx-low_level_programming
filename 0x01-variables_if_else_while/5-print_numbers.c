bce900381535457bd4c6d90bc33dc6ff0a4797bf
calebtoo <calebtoo77@gmail.com>
2022-09-10 14:39:06 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - numbers
+ * Return: 0 (Always Success)
+ */
+
+int main(void)
+{
+	int n= 0;
+	
+	while (0 < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}	
