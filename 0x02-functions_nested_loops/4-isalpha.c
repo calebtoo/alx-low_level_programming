d095c209328a720bd6bfa0f393a7e19aa02dcfc5
calebtoo <calebtoo77@gmail.com>
2022-09-14 08:06:23 -0700

    Conditional II

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if alphabet
+ * @c: character to check
+ * Return 1 if a letter, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A'; c >= 'a'; && c <= 'Z' c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
