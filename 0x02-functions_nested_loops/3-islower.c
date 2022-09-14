1541ade0e571f27e32b8d644ca35df526eb33eb8
calebtoo <calebtoo77@gmail.com>
2022-09-14 07:41:32 -0700

    Conditional

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - prints result if lowercase
+ * Return: 0 or 1
+ */
+
+int _islower(void)
+{
+	if (c >= 'a') && (c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n')
+}
