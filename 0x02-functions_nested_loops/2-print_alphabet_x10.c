3ed68b63b5045d6188f11e9d5b5c1f50c7551603
calebtoo <calebtoo77@gmail.com>
2022-09-14 07:30:24 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: 0 (Always Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha)
+		}
+		_putchar('\n')
+	}
+}
