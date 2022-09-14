d63d5065396741d3ec9706116f987f8d657edbe1
calebtoo <calebtoo77@gmail.com>
2022-09-14 08:37:27 -0700

    Conditional III

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _print_sign - prints the sign
+ * @n: checks number
+ * Return: 1 and prints + if n greater than 0, 0 and prints 0 if n is 0, -1 and prints - if n is less than 0
+ */
+
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		Return (1);
+		_putchar('+');
+	}
+	else if (n ==0)
+	{
+		Return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		Return (-1);
+		_putchar('-');
+	}
+}
+		
