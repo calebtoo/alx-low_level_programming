8f8bfdc3a622c606c70cd3182f9efec4be1b8e96
calebtoo <calebtoo77@gmail.com>
2022-09-14 06:44:14 -0700

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ * 
+ * Description - lowercase alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+	alpha = 'a';
+
+	while(alpha <= 'z');
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	Return(0);
+}
