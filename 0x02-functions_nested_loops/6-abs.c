6f3fc5cedcd558e686a62d01d7530e22664b6f83
calebtoo <calebtoo77@gmail.com>
2022-09-14 08:58:16 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *abs - compute absolute value
+ *@n: integer value
+ *Return: absolute value
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	return (0);
+}
