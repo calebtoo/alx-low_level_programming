f511e2ac043edecd1542befadf530fa4bed5b69e
calebtoo <calebtoo77@gmail.com>
2022-09-19 15:18:47 -0700

    Copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = o;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte
+ * to the buffer pointed to by dest
+ * @dest: coopy source to this buffer
+ * @src: source to copy
+ * Return: copy of the original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return (dest);
+}
