da3bdf048a08fa3cb6673d5d9a8884369e36e618
calebtoo <calebtoo77@gmail.com>
2022-09-19 14:40:50 -0700

    Reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * rev_string - reverses a string
  * @s: string to reverse
  */
