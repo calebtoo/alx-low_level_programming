ebb268a493dcebd7d4524f1f89adcc49e1e743b7
calebtoo <calebtoo77@gmail.com>
2022-09-22 05:40:25 -0700

    Concatenate II

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
