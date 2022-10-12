2c1a03c96a1c32a02e25fc12bf29e05ca598ddc4
calebtoo <calebtoo77@gmail.com>
2022-10-12 13:20:30 -0700

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
-
 struct dog
 {
 	char *name;
@@ -12,5 +9,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif
