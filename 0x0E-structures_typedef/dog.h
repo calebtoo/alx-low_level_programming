372d2c1ac334644a384cd5b94bd7dc1e371f5f1c
calebtoo <calebtoo77@gmail.com>
2022-10-12 14:06:12 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,21 @@
+/**
+ * struct dog - best friend to humankind
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner's name
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedeg for dog structure
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
