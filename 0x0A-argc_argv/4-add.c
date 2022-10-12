f59f9cda56d558f4505d3e64935a52bacd1ba0b2
calebtoo <calebtoo77@gmail.com>
2022-10-12 05:33:23 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,24 @@
 #include <stdbool.h>
 
 /**
+ * is_num - iterate through each argb to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool is_num(char *argvv)
+{
+	int j = 0;
+	
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * main - print sum if all arguments given are numbers
  * @argc: argument counter
  * @argv: arguments
