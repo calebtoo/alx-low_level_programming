b436baf3d2b68899c7534167b11ecbb02f69d555
calebtoo <calebtoo77@gmail.com>
2022-10-25 04:49:08 -0700

    List

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> /* size_t, printf */
+#include <stdlib.h> /* malloc */
+#include <string.h> /* strdup */
 
 /**
  * struct list_s - singly linked list
